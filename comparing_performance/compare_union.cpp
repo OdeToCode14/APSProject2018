0ab85b5260091d32e3370240c86fee577452cf7c
amit tiwari <amit.tiwari@students.iiit.ac.in>
2018-11-10 03:09:12 +0530

    changes made

---
+++ comparing_performance/compare_union.cpp
@@ -0,0 +1,72 @@
+#include <bits/stdc++.h>
+
+#include "hash_set.h"
+typedef long long ll;
+using namespace std;
+
+int main(){
+    /*int first=10;
+    int second=6;
+    vector<int> arr1(first);
+
+    ifstream in1;
+    in1.open("test_file1.txt");
+    for(int i=0;i<first;i++){
+        ll x;
+        in1>>x;
+        arr1[i]=x;
+    }
+
+    vector<int> arr2(second);
+
+    ifstream in2;
+    in1.open("test_file2.txt");
+    for(int i=0;i<second;i++){
+        ll x;
+        in2>>x;
+        arr2[i]=x;
+    }
+// test for time from here
+    hash_set<int> st1;
+	for(int i=0;i<first;i++){
+        st1.insert(arr1[i]);
+    }
+    hash_set<int> st2;
+	for(int i=0;i<second;i++){
+        st2.insert(arr2[i]);
+    }
+    hash_set<int> st3=st1.find_union(st2);
+    cout<<st3.occupied<<"\n";*/
+
+
+    int left=1,right=10000;
+    int pos=0;
+	vector<int> arr1(right-left+1);
+	for(int i=left;i<=right;i++){
+		arr1[pos++]=i;
+	}
+	random_shuffle(arr1.begin(),arr1.end());
+	
+
+  	left=1000,right=100000;
+    vector<int> arr2(right-left+1);
+    pos=0;
+    for(int i=left;i<=right;i++){
+		arr2[pos++]=i;
+	}
+	random_shuffle(arr2.begin(),arr2.end());
+// test for time from here
+    hash_set<int> st1;
+	for(int i=0;i<arr1.size();i++){
+        st1.insert(arr1[i]);
+    }
+    hash_set<int> st2;
+	for(int i=0;i<arr2.size();i++){
+        st2.insert(arr2[i]);
+    }
+    hash_set<int> st3=st1.find_union(st2);
+    st3.print();
+    //cout<<st3.occupied<<"\n";
+
+	return 0;
+}
---
+++ test_files/generate_test.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <fstream>
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main () {
+ /* ofstream file;
+  file.open ("test_file.txt");
+  for(int i=0;i<10;i++){
+  	file<<i<<"\n";
+  }
+  //file << "Please writr this text to a file.\n this text is written using C++\n";
+  file.close();
+  */
+	int left=1,right=10;
+  int pos=0;
+	vector<int> arr(right-left+1);
+	for(int i=left;i<=right;i++){
+		arr[pos++]=i;
+	}
+	random_shuffle(arr.begin(),arr.end());
+	
+  ofstream file;
+  file.open ("test_file1.txt");
+  for(int i=0;i<arr.size();i++){
+	 file<<arr[i]<<"\n";
+  }
+
+  //file << "Please writr this text to a file.\n this text is written using C++\n";
+  file.close();
+
+  left=5,right=10;
+  vector<int> arr2(right-left+1);
+  pos=0;
+  for(int i=left;i<=right;i++){
+    arr2[pos++]=i;
+  }
+  random_shuffle(arr2.begin(),arr2.end());
+  
+  ofstream file2;
+  file2.open ("test_file2.txt");
+  for(int i=0;i<arr2.size();i++){
+    file2<<arr2[i]<<"\n";
+  }
+
+  //file << "Please writr this text to a file.\n this text is written using C++\n";
+  file.close();
+
+  return 0;
+}
